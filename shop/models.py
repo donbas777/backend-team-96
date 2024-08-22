7d3200cedf21fddc04dbba2b7f57b28f4311b943
donbas777 <renary34@gmail.com>
2024-08-22 21:14:17 +0300

    updating

--- shop/models.py
+++ shop/models.py
@@ -9,7 +9,7 @@ from multiselectfield import MultiSelectField
 
 def embroidery_image_file_path(instance, filename):
     _, extension = os.path.splitext(filename)
-    filename = f"{slugify(instance.title)}-{uuid.uuid4()}{extension}"
+    filename = f"{slugify(instance.name)}-{uuid.uuid4()}{extension}"
 
     return os.path.join("uploads/embroidery/", filename)
 
