4bc024dfc305a3ed58adcf5838e24dbaff237ae0
donbas777 <renary34@gmail.com>
2024-08-23 13:41:20 +0300

    update user/models.py

--- user/models.py
+++ user/models.py
@@ -43,6 +43,9 @@ class UserManager(BaseUserManager):
 class User(AbstractUser):
     username = None
     email = models.EmailField(_("email address"), unique=True)
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    phone_number = models.CharField(max_length=15, blank=True, null=True)
 
     USERNAME_FIELD = "email"
     REQUIRED_FIELDS = []
